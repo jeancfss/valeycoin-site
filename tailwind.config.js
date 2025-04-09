a8371597e446ac653b46ee47e9742a60648f7870
Jean Ferreira <92958481+jeancfss@users.noreply.github.com>
2025-04-08 21:06:15 -0300

    Subindo projeto ValeyCoin com Tailwind configurado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
